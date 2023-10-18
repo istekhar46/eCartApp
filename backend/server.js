224367b1a0521f1cf87642b3981eba115ecc20a9
istekhar khan <istekharkhan46@gmail.com>
2023-10-19 00:31:29 +0530

    initial setup

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.get('/', (req, res) => {
+    res.send('Server is ready');
+})
+
+app.listen(PORT, () => console.log(`Server is running on port http://localhost:${PORT}`))
---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="container shadow-md">
+        <h1 className="text-primaryColor font-bold">Hello Ecart</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
